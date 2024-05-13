d002e716f1fcdba5ffe9a3b8210699c2eceb1d8e
Filomeno Sanchez <filomeno.sanchezrodriguez@york.ac.uk>
2024-05-13 17:20:33 +0100

    fix gallery modal text colour

--- src/components/layouts/GalleryLayout.tsx
+++ src/components/layouts/GalleryLayout.tsx
@@ -53,7 +53,7 @@ export const GalleryLayout: React.FC = () => {
         <div style={{width: '99vw', overflowX: 'hidden', overflowY: 'auto'}}>
           <Gallery images={images} enableImageSelection={false} onClick={handleFigureClick}/>
         </div>
-        <Modal open={showModal} onClose={() => setShowModal(false)} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description" style={{backdropFilter: "blur(5px)"}}>
+        <Modal open={showModal} onClose={() => setShowModal(false)} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description" style={{color: 'black', backdropFilter: "blur(5px)"}}>
         <Box sx={{
             display: 'flex',
             justifyContent: 'center',
