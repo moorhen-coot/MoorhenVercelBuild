4a45a8922dd36d14832de4d27e6967432fa2ba74
Stuart McNicholas <stuart.mcnicholas@york.ac.uk>
2024-11-08 13:13:37 +0000

    CODLayout depends on moorhen small_mol_rdkit 9cdc8842f46e61604d319392e0753cdcb0d57855

--- src/components/layouts/CODLayout.tsx
+++ src/components/layouts/CODLayout.tsx
@@ -1,6 +1,7 @@
 import { MoorhenContainer, MoorhenMolecule, MoorhenReduxStore, addMolecule } from 'moorhen'
 import { webGL } from 'moorhen/types/mgWebGL';
 import { moorhen } from 'moorhen/types/moorhen';
+import { libcootApi } from "moorhen/types/libcoot"
 import { useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -9,7 +10,6 @@ export const CODLayout: React.FC = () => {
     const dispatch = useDispatch()
     const cootInitialized = useSelector((state: moorhen.State) => state.generalStates.cootInitialized)
     const defaultBondSmoothness = useSelector((state: moorhen.State) => state.sceneSettings.defaultBondSmoothness)
-    const backgroundColor = useSelector((state: moorhen.State) => state.sceneSettings.backgroundColor)
 
     const glRef = useRef<webGL.MGWebGL | null>(null)
     const commandCentre = useRef<moorhen.CommandCentre | null>(null)
@@ -19,30 +19,58 @@ export const CODLayout: React.FC = () => {
     const urlPrefix = "/baby-gru"
     //const baseUrl = 'https://www.crystallography.net/cod'
     const baseUrl = '/cod'
-    const monomerLibraryPath = 'https://raw.githubusercontent.com/MRC-LMB-ComputationalStructuralBiology/monomers/master'
 
     const loadCOD = async (codid: string) => {
-        const url = `${baseUrl}/${codid}.cif`
-        const newMolecule = new MoorhenMolecule(commandCentre, glRef, MoorhenReduxStore, monomerLibraryPath)
-        newMolecule.setBackgroundColour(backgroundColor)
-        newMolecule.defaultBondOptions.smoothness = defaultBondSmoothness
-        try {
-            const response = await fetch(url)
-            if (response.ok) {
-                const coordData = await response.text()
-                await newMolecule.loadToCootFromString(coordData, codid, true)
-            }
-            if (newMolecule.molNo === -1) {
-                throw new Error("Cannot read the fetched molecule...")
-            } 
-            await newMolecule.fetchIfDirtyAndDraw('CBs')
-            await newMolecule.addRepresentation('ligands', '/*/*/*/*')
-            await newMolecule.centreOn('/*/*/*/*', true, true)
-            dispatch(addMolecule(newMolecule))
-        } catch (err) {
-            console.warn(err)
-            console.warn(`Cannot fetch PDB entry from ${url}, doing nothing...`)
+
+        if (!glRef.current || !commandCentre.current) {
+            console.warn('Empty glRef or commandCentre, doing nothing...')
+            return
+        } else if (!codid) {
+            console.warn('Empty codid string, doing nothing...')
+            return
         }
+
+        const url = `${baseUrl}/${codid}.cif`
+            const response = await fetch(url)
+            //if (response.ok) {
+            const coordData = await response.text()
+
+        const cod_to_cif_response = await commandCentre.current.cootCommand({
+            command: 'SmallMoleculeCifToMMCif',
+            commandArgs: [coordData],
+            returnType: 'str_str_pair'
+        }, true) as moorhen.WorkerResponse<libcootApi.PairType<string, string>>
+        const dictContent = cod_to_cif_response.data.result.result.second
+
+        const anyMolNo = -999999
+        const newMolecule = new MoorhenMolecule(commandCentre, glRef, MoorhenReduxStore, baseUrl)
+
+        await commandCentre.current.cootCommand({
+            returnType: "status",
+            command: 'read_dictionary_string',
+            commandArgs: [dictContent, anyMolNo],
+        }, false)
+
+        const result = await commandCentre.current.cootCommand({
+            returnType: 'status',
+            command: 'get_monomer_and_position_at',
+            commandArgs: ["UNK", anyMolNo,
+                ...glRef.current.origin.map(coord => -coord)
+            ]
+        }, true) as moorhen.WorkerResponse<number>
+
+        if (result.data.result.status === "Completed" && result.data.result.result !== -1) {
+            newMolecule.molNo = result.data.result.result
+            newMolecule.name = "LIG"
+            newMolecule.setBackgroundColour(glRef.current.background_colour)
+            newMolecule.defaultBondOptions.smoothness = defaultBondSmoothness
+            newMolecule.coordsFormat = 'mmcif'
+            await newMolecule.fetchIfDirtyAndDraw("CBs")
+            dispatch( addMolecule(newMolecule) )
+        } else {
+            console.warn('Error getting monomer... Missing dictionary?')
+        }
+
     }
 
     useEffect(() => {
